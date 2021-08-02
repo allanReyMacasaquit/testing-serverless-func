8db9907ed6ec9ec239663464d9964a162fa29cfc
Cherry Macasaquit <allanrey.macasaquit@gmail.com>
2021-08-02 15:11:34 -0400

    testing deploy

---
+++ examples/1-hello/app.js
@@ -0,0 +1,13 @@
+const result = document.querySelector(".result");
+
+const fetchData = async () => {
+  try {
+    // const { data } = await axios.get("/.netlify/functions/hello");
+    const { data } = await axios.get("/api/hello");
+    result.textContent = data;
+  } catch (error) {
+    console.log(error.response);
+    result.textContent = error.response.data;
+  }
+};
+fetchData();
