bbe95523a012e98638e9a8c3fa8c90f572a08a2f
Cherry Macasaquit <allanrey.macasaquit@gmail.com>
2021-08-02 18:07:08 -0400

    fixed error

--- functions/basic-api.js
+++ functions/basic-api.js
@@ -3,7 +3,8 @@ const items = require("../assets/data");
 exports.handler = async (event, context) => {
   return {
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin":
+        "https://serverless-vanilla.netlify.app/api/basic-api",
     },
     statusCode: 200,
     body: JSON.stringify(items),
